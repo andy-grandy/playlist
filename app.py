6f4b7a41ebcbab7bbf540d2d7b47ec99f85c9795
Andrey Tarakanov <atarakanov@yotateam.com>
2018-09-28 16:37:43 +0300

    test

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from bottle import run, static_file, route, default_app
 from playmaker import makepl, getjson
 
-makepl()
-
 @route('/<filepath:path>')
 def server_static(filepath):
     return static_file(filepath, root='static')
@@ -12,4 +10,4 @@ def maker():
     getjson()
     makepl()
 
-application = default_app()
+app = default_app()
